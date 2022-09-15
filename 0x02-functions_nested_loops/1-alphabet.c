adbc0ea21079662afd9e5a8ee633bd1ee3d7aa1e
“Osinachi” <osinachichiawa@gmail.com>
2022-09-14 18:18:32 -0700

    Lowercase Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: print_alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+
+{
+
+int ch;
+for (ch = 'a'; ch <= 'z'; ++ch)
+_putchar(ch);
+_putchar('\n');
+
+
+}
