d5b64023fa3574ff12e30e6fe67137587267973d
“Osinachi” <osinachichiawa@gmail.com>
2022-09-14 17:45:26 -0700

    Putting Character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char str [] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+
+return (0);
+
+
+}
