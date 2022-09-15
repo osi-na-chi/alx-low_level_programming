81ea772b2a02fcba89d554947d37a601cad3aa15
“Osinachi” <osinachichiawa@gmail.com>
2022-09-14 22:05:05 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (hr = 0; hr <= 23; ++hr)
 for (min = 0; min <= 59; ++min)
 {
 _putchar((hr / 10) + 48);
-_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
 _putchar(':');
 _putchar((min / 10) + 48);
 _putchar((min % 10) + 48);
