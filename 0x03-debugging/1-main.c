b58be4eb510fe9bb3275253171f30fe20b729adf
“Osinachi” <osinachichiawa@gmail.com>
2022-09-16 11:37:14 -0700

    Confusing

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - creates an infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming : (\n");
 
 i = 0;
@@ -18,5 +19,6 @@ i = 0;
  * }
  */
 printf("infinite loop evaded! \\o/\n");
+
 return (0);
 }
