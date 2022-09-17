dd50b4949a44177dafaefb71252ac461a8ca9aa9
“Osinachi” <osinachichiawa@gmail.com>
2022-09-16 20:40:00 -0700

    This may take some getting used to

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_line - function that draws a straight line in the terminal
+ *@n: parameter
+ *Return: returns nothing
+ */
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+putchar('_');
+}
+putchar('\n');
+}
+
