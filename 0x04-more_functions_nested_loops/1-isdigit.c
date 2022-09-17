94ae50b3ee5a1a2db1e1a2a0116937d793e78dae
“Osinachi” <osinachichiawa@gmail.com>
2022-09-16 18:00:40 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - function that checks for a digit
+ *
+ * @c: character to print
+ *
+ * Return: 0 on success and 1 on fail
+ */
+
+int _isdigit(int c)
+{
+int i = _isdigit(c);
+if (i > 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
