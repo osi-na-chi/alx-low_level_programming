925004c2a0b08845b8aae0d2d41140aa2cc74bb7
“Osinachi” <osinachichiawa@gmail.com>
2022-09-20 18:18:41 -0700

    The length of the string returns

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - function that returns the length of a string
+ *@s: string parameter input
+ *Return:Length of string
+ */
+int _strlen(char *s)
+{
+int counter;
+for (counter = 0; *s != '\0'; ++s)
+++counter;
+return (counter);
+}
