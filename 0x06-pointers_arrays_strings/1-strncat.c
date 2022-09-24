1e15eda7bef3f819a3b96b95b3c6a7bb9789734a
“Osinachi” <osinachichiawa@gmail.com>
2022-09-23 18:02:24 -0700

    Lets go

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *strncat - function that concatenates two strings
+ *@dest: This is pointer to the destination array
+ *@src: This is the string to be appended
+ *@n: integer
+ *Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int 
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x06-pointers_arrays_strings
+This is way more complex and intersting than the first project
