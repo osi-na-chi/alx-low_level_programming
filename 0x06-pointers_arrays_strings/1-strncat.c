90cd2115e1ee50dab35c0401324d103426a950f9
“Osinachi” <osinachichiawa@gmail.com>
2022-09-23 19:26:24 -0700

    I will push through

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
- *strncat - function that concatenates two strings
+ *_strncat - function that concatenates two string
  *@dest: This is pointer to the destination array
- *@src: This is the string to be appended
- *@n: integer
+ *@src: this is the string to be appended
+ *@n: This is the maximum number of characters to be appended
  *Return: a pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int 
+int i = 0, j = 0;
+while (*(dest + i))
+{
+i++;
+}
+while (*(src + j) != '\0' && j < n)
+{
+*(dest + i) = src[j];
+i++;
+j++;
+}
+return (dest);
 }
