e19b1f43fce77221ff500ff7b6864b7a807b50f4
“Osinachi” <osinachichiawa@gmail.com>
2022-09-29 21:35:44 -0700

    Finding the natural square roots is a little confusing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int sqrt_check(int g, int c)
 {
 if (g * g == c)
 return (g);
-if (g * g < c)
+if (g * g > c)
 return (-1);
 return (sqrt_check(g + 1, c));
 }
@@ -24,7 +24,5 @@ return (sqrt_check(g + 1, c));
 
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-return (0);
 return (sqrt_check(1, n));
 }
