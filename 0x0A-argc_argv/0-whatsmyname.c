e8ed1f756e68850827a4def91f71000d7c78978d
“Osinachi” <osinachichiawa@gmail.com>
2022-10-04 19:27:14 -0700

    Printed ot the name already

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *argc: counts the arguments
- *argv: pointer to a string of arrays
+ *@argc: counts the arguments
+ *@argv: pointer to a string of arrays
  *Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
