55369ab15ad2710b8ad0843ded798ae3739a310f
“Osinachi” <osinachichiawa@gmail.com>
2022-10-04 18:58:11 -0700

    Printed out the name already

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *argc - counts the arguments
+ *argv - pointer to a string of arrays
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+It is kind of a hard thing to process, infact all things are hard to process
