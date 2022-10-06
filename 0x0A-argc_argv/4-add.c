7bb837fba4e12d7e418168832d132c34e77c455e
“Osinachi” <osinachichiawa@gmail.com>
2022-10-05 20:16:00 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - adds two positive numbers
+ *@argc: counts the arguments
+ *@argv: pointer to a string of arrays
+ *Return: 0 if no errors, else 1
+ */
+
+int main(int argc, char *argv[])
+{
+int a = 0, i, j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (isdigit(argv[i][j]) == 0)
+{
+puts("Error");
+return (1);
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+a += atoi(argv[i]);
+}
+printf("%d\n", a);
+return (0);
+}
