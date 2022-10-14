09426fa654951620da59babebb49c1a177c2f949
“Osinachi” <osinachichiawa@gmail.com>
2022-10-14 15:09:45 -0700

    Help Lord

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,20 @@
 #define _DOG_H_
 
 /**
- *struct dog - making a dog
- *@name: name
- *@age: dog age
- *@owner: parent of the dog
- *Description: has all attribute in a box
- */
+* struct dog - structure for a basic dog
+*@name: Dog's name
+*@age: Dog's age
+*@owner: Dog's owner
+*Description: data structure for dog
+*/
 
 struct dog
 {
 char *name;
 float age;
-char *owner
+char *owner;
 };
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
