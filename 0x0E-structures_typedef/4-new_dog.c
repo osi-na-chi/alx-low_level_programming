037d791929e2a60abb2152766875e2c4d8d45e52
“Osinachi” <osinachichiawa@gmail.com>
2022-10-14 18:42:04 -0700

    Project 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  *@name: Dog's name
  *@age: Dog's age
  *@owner: Dog's owner
- *Return: NULL if the function fails
+ *Return: returns a pointer to newly created dog structure
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
